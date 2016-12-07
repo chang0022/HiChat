1701f044732c24bbfddee6c40ffc7b3fdbfcf0df
chang0022 <chang.0022@163.com>
2016-12-07 20:02:38 +0800

    socket.io

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 var express = require('express'),
     app     = express(),
-    server  = require('http').createServer(app);
+    server  = require('http').createServer(app),
+    io = require('socket.io').listen(server);
 
 app.use('/', express.static(__dirname + '/www'));
 server.listen(8090);
-console.log('server started');
+
+io.on('connection', function(socket) {
+    socket.on('foo', function(data) {
+        console.log(data);
+    });
+});
